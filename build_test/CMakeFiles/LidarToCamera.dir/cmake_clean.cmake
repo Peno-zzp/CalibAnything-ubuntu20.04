4e6bd70e56e9686f24ab6d3c1bcbbe622568eceb
zpzhang <894243423@qq.com>
2025-04-06 06:57:44 -0700

    Initial commit

---
+++ build_test/CMakeFiles/LidarToCamera.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/libLidarToCamera.a"
+  "../lib/libLidarToCamera.pdb"
+  "CMakeFiles/LidarToCamera.dir/src/calibration.cpp.o"
+  "CMakeFiles/LidarToCamera.dir/src/run_lidar2camera.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LidarToCamera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
